487ffd17e4d45cb51ecf48e81644d165f93ce842
jamari-morrison <RedCater360@yahoo.com>
2021-05-02 09:10:48 -0400

    Added process experiment objects in UI

---
+++ src/src/PresentationLayer/ProcessReagentExperiment.java
@@ -0,0 +1,13 @@
+package PresentationLayer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class ProcessReagentExperiment implements Cmd{
+    String sampleID;
+    
+    @Override
+    public boolean execute(BufferedReader reader) throws IOException {
+        return false;
+    }
+}
---
+++ src/src/PresentationLayer/ProcessSampleExperiment.java
@@ -0,0 +1,11 @@
+package PresentationLayer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class ProcessSampleExperiment implements Cmd{
+    @Override
+    public boolean execute(BufferedReader reader) throws IOException {
+        return false;
+    }
+}
