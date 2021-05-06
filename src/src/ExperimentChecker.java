19764fcb5a651cb42e00431ac10a66affaac84f6
jamari-morrison <RedCater360@yahoo.com>
2021-05-06 15:06:17 -0400

    added experiment checker to show experiments are valid

---
+++ src/src/ExperimentChecker.java
@@ -0,0 +1,26 @@
+public class ExperimentChecker {
+
+
+    public ExperimentChecker(){
+
+    }
+
+    public boolean checkSampleExperiment(SampleExperiment toCheck){
+        //take in experiment object
+        return false;
+    }
+
+    public boolean checkReagentExperiment(ReagentExperiment toCheck){
+
+
+        return false;
+    }
+
+    public boolean checkComplexExperiment(ComplexExperiment toCheck){
+
+
+        return false;
+    }
+
+
+}
