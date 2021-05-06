f8ca1b88f2e2644c4aeadb912834b7923f7a08de
jamari-morrison <RedCater360@yahoo.com>
2021-05-06 16:12:32 -0400

    Added logic to ExperimentChecker (need to have the database connection actually interface with that logic, though)

--- src/src/ExperimentChecker.java
+++ src/src/ExperimentChecker.java
@@ -5,19 +5,28 @@ public class ExperimentChecker {
 
     }
 
-    public boolean checkSampleExperiment(SampleExperiment toCheck){
-        //take in experiment object
+    public boolean checkSampleExperiment(){
+        //Just return if status is "Operational" for all of these (in database)
+        //check Arm-1
+        //check Arm-2
+        //check Arm-3
+        //check test tube available quantity
+        //check ejector
         return false;
     }
 
-    public boolean checkReagentExperiment(ReagentExperiment toCheck){
-
-
+    public boolean checkReagentExperiment(String reagent, String sample){
+        //check Arm-2
+        //check pipet
+        //check reagent quantity
+        //check sample quantity
         return false;
     }
 
-    public boolean checkComplexExperiment(ComplexExperiment toCheck){
-
+    public boolean checkComplexExperiment(String supplyItem, String supplyQuantity, String sample){
+        //check summplyItem quantity
+        //check sample quantity
+        //check based on verb?
 
         return false;
     }
