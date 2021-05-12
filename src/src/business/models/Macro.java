1a01840bebc78d5782d683afde8e588e3d41a6b7
snidersa <snidersa@rose-hulman.edu>
2021-05-12 17:10:56 -0400

    safdsdaf

---
+++ src/src/business/models/Macro.java
@@ -0,0 +1,10 @@
+package business.models;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Macro {
+    List<Command> commands = new ArrayList<>();
+}
